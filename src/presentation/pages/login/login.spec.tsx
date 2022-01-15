0d840a86f641f7ccbdc271b4f7c76ba3d6813d0b
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-01-15 07:44:52 -0300

    feat: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -48,4 +48,12 @@ describe('Login component ', () => {
     fireEvent.input(emailInput, { target: { value: email } })
     expect(validationSpy.input).toEqual({ email })
   })
+
+  test('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const password = faker.internet.password()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: password } })
+    expect(validationSpy.input).toEqual({ password })
+  })
 })
