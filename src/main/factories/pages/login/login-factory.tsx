13647905fc550c92a5b9ebc0a4c42137722533ba
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-01-17 19:59:29 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios.http-client'
-import { ValidationComposite, ValidationBuilder } from '@/validation/validators'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = 'http://fordevs.herokuapp.com/api/login'
-  const axiosHttpCliente = new AxiosHttpClient()
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpCliente)
-  const validationComposite = new ValidationComposite([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
   return (
     <Login
-    authentication={remoteAuthentication}
-    validation={validationComposite}
+    authentication={makeRemoteAuthentication()}
+    validation={makeLoginValidation()}
     />
   )
 }
---
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -0,0 +1,8 @@
+import { ValidationComposite, ValidationBuilder } from '@/validation/validators'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  return new ValidationComposite([
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build()
+  ])
+}
