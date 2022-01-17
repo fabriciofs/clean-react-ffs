405d4d4fbd2e6c728fe9ad35f4dbee4f5d9dbafd
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-01-17 17:55:43 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,19 +1,35 @@
+import faker from 'faker'
 import { ValidationBuilder as sut } from './validation-builder'
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    const fieldName = faker.database.column()
+    const validations = sut.field(fieldName).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
   })
 
   test('Should return EmailFieldValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const fieldName = faker.database.column()
+    const validations = sut.field(fieldName).email().build()
+    expect(validations).toEqual([new EmailValidation(fieldName)])
   })
 
   test('Should return MinLengthFieldValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    const fieldName = faker.database.column()
+    const minLength = faker.datatype.number()
+    const validations = sut.field(fieldName).min(minLength).build()
+    expect(validations).toEqual([new MinLengthValidation(fieldName, minLength)])
+  })
+
+  test('Should return a list of validations', () => {
+    const fieldName = faker.database.column()
+    const minLength = faker.datatype.number()
+    const validations = sut.field(fieldName).required().min(minLength).email().build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(fieldName),
+      new MinLengthValidation(fieldName, minLength),
+      new EmailValidation(fieldName)
+    ])
   })
 })
