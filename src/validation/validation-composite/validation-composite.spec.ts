4ae40d6d6125a0b294cc66256befca5814b393b1
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-01-17 17:07:15 -0300

    refactor: SUT creation

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,22 +1,27 @@
 import { ValidationComposite } from './validation-composite'
-import { FieldValidation } from '@/validation/protocols/field-validation'
 import { FieldValidationSpy } from '@/validation/test'
 
-const makeSut = (validators: FieldValidation[]): ValidationComposite => {
-  const sut = new ValidationComposite(validators)
-  return sut
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValitationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationSpy1 = new FieldValidationSpy('any_field')
+  const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+  const fieldValitationsSpy = [
+    fieldValidationSpy1,
+    fieldValidationSpy2
+  ]
+  const sut = new ValidationComposite(fieldValitationsSpy)
+  return { sut, fieldValitationsSpy }
 }
 
 describe('ValidationComposite', () => {
   test('Shoud return error if any validation fails', () => {
-    const fieldValidationSpy1 = new FieldValidationSpy('any_field')
-    fieldValidationSpy1.error = Error('first_error_message')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = Error('second_error_message')
-    const sut = makeSut([
-      fieldValidationSpy1,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValitationsSpy } = makeSut()
+    fieldValitationsSpy[0].error = Error('first_error_message')
+    fieldValitationsSpy[1].error = Error('second_error_message')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('first_error_message')
   })
